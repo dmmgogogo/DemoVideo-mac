dfd19f1aa5f509ae565d4f5a51d27e30ae749a71
jerry <jerry@outlook.com>
2025-07-31 16:31:13 +0800

    Initial Commit

---
+++ DemoVideo-mac/DemoVideo_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoVideo_macApp.swift
+//  DemoVideo-mac
+//
+//  Created by mmx on 2025/7/31.
+//
+
+import SwiftUI
+
+@main
+struct DemoVideo_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
